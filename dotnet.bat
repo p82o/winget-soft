6f2ed4a951478770f725672b40a09ae421edfb83
osipovpa <p82o@yandex.ru>
2023-01-27 23:47:21 +0300

    initial commit

---
+++ dotnet.bat
@@ -0,0 +1,4 @@
+winget install -e --id Microsoft.DotNet.DesktopRuntime.3_1
+winget install -e --id Microsoft.DotNet.DesktopRuntime.5
+winget install -e --id Microsoft.DotNet.DesktopRuntime.6
+winget install -e --id Microsoft.DotNet.DesktopRuntime.7
---
+++ vcredist.bat
@@ -0,0 +1,12 @@
+winget install -e --id Microsoft.VCRedist.2005.x86
+winget install -e --id Microsoft.VCRedist.2005.x64
+winget install -e --id Microsoft.VCRedist.2008.x86
+winget install -e --id Microsoft.VCRedist.2008.x64
+winget install -e --id Microsoft.VCRedist.2010.x86
+winget install -e --id Microsoft.VCRedist.2010.x64
+winget install -e --id Microsoft.VCRedist.2012.x86
+winget install -e --id Microsoft.VCRedist.2012.x64
+winget install -e --id Microsoft.VCRedist.2013.x86
+winget install -e --id Microsoft.VCRedist.2013.x64
+winget install -e --id Microsoft.VCRedist.2015+.x86
+winget install -e --id Microsoft.VCRedist.2015+.x64
