9afcb61f6b4a43ff381405b646db3ea7d442a3e2
osipovpa <p82o@yandex.ru>
2023-10-17 11:16:55 +0300

    add AspNetCore

--- scripts/dotnet.bat
+++ scripts/dotnet.bat
@@ -1,4 +1,8 @@
+winget install -e --id Microsoft.DotNet.AspNetCore.3_1
 winget install -e --id Microsoft.DotNet.DesktopRuntime.3_1
+winget install -e --id Microsoft.DotNet.AspNetCore.5
 winget install -e --id Microsoft.DotNet.DesktopRuntime.5
+winget install -e --id Microsoft.DotNet.AspNetCore.6
 winget install -e --id Microsoft.DotNet.DesktopRuntime.6
+winget install -e --id Microsoft.DotNet.AspNetCore.7
 winget install -e --id Microsoft.DotNet.DesktopRuntime.7
